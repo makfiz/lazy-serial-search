51acc097912ca9249f6ac23dd46924da031cb5fb
leda_oleksandr <oleda@highedgesoft.com>
2022-10-30 21:18:07 +0200

    Added saveHistory and getUserTrailers

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,38 @@
-import { Box } from "components/utils/Box";
-
+import trailers from "components/utils/test.json";
 
 export const App = () => {
-  return (
-    <Box>
+  function saveHistory (trailer = null) {
+    let historyTrailersId = localStorage.getItem('history');
+    if (null !== historyTrailersId) {
+      historyTrailersId = JSON.parse(historyTrailersId);
+      if (!historyTrailersId.includes(trailer.id)) {
+        historyTrailersId.push(trailer.id);
+        localStorage.setItem('history', JSON.stringify(historyTrailersId));
+      }
+    } else {
+      if (null !== trailer) {
+        localStorage.setItem('history', JSON.stringify([trailer.id]));
+      }
+    }
+  }
 
-    </Box>
+  function getUserTrailers () {
+    let historyTrailersId = JSON.parse(localStorage.getItem('history'));
+    let userTrailers = [];
+    trailers.forEach(trailer => {
+      if (!historyTrailersId.find(id => id === trailer.id)) {
+        userTrailers.push(trailer);
+      }
+    });
+
+    console.log(userTrailers);
+  }
+
+  saveHistory({'id': 3})
+  getUserTrailers()
+
+  return (
+    <div>
+    </div>
   );
 };
