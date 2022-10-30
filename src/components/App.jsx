33095e9b26221b500c37af14cd7134d4b73d1484
Maksym Hlanskov <makfiz@gmail.com>
2022-10-30 21:24:14 +0200

    1st

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,21 @@
 import { Box } from "components/utils/Box";
+import test from 'components/utils/test.json'
+import Btn from 'components/Btn/Btn'
+import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
 
 
 export const App = () => {
+  console.log(test)
   return (
-    <Box>
-
-    </Box>
+    <>
+      <Box
+      display="flex"
+      justifyContent="space-between"
+      >
+        <Btn><IoIosArrowBack size={70} /></Btn>
+        <Box width ="800px" height ="800px"></Box>
+      <Btn><IoIosArrowForward size={70}/></Btn>
+      </Box>
+    </>
   );
 };
---
+++ src/components/Btn/Btn.js
@@ -0,0 +1,12 @@
+import { Button } from 'components/Btn/Btn.styled'
+
+const Btn = ({ children }) => {
+
+    return (
+        <Button>
+            {children}
+        </Button>
+    )
+}
+
+export default Btn
