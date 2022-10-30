f7b9e596622c3484942ffe9940857425f05765d7
Maksym Hlanskov <makfiz@gmail.com>
2022-10-30 19:50:34 +0200

    hg

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 import { ThemeProvider } from 'styled-components';
-import {theme} from 'components/utils/theme'
+import { theme } from 'components/utils/theme'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ThemeProvider theme={theme}>
   <React.StrictMode>
