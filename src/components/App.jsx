62f96e24c211aac2bd115d8b46740b8477e7a759
leda_oleksandr <oleda@highedgesoft.com>
2022-10-30 22:57:22 +0200

    MVP 1.0

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,46 +1,35 @@
 import trailers from "components/utils/test.json";
-import { Box } from "components/utils/Box";
+import {Box} from "components/utils/Box";
 import Btn from 'components/Btn/Btn'
-import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
-import { useState, useEffect } from "react";
-
-
-
+import {IoIosArrowBack, IoIosArrowForward} from 'react-icons/io';
+import {useState, useEffect} from "react";
 
 
 export const App = () => {
   const [curTrailer, setCurTrailer] = useState(null)
 
-  // useEffect(() => {
-      
+  useEffect(() => {
+    getRandomTrailer()
+  },[])
 
-  // },[curTrailer])
-
-    function saveHistory(trailer = null) {
-      if (trailer === null) return 
-      let historyTrailersId = localStorage.getItem('history');
-      if (null !== historyTrailersId) {
-        historyTrailersId = JSON.parse(historyTrailersId);
-        if (!historyTrailersId.includes(trailer.id)) {
-          historyTrailersId.push(trailer.id);
-          localStorage.setItem('history', JSON.stringify(historyTrailersId));
-        }
-      } else {
-        if (null !== trailer) {
-          localStorage.setItem('history', JSON.stringify([trailer.id]));
-        }
+  function saveHistory(trailer = null) {
+    if (trailer === null) return
+    let historyTrailersId = localStorage.getItem('history');
+    if (null !== historyTrailersId) {
+      historyTrailersId = JSON.parse(historyTrailersId);
+      if (!historyTrailersId.includes(trailer.id)) {
+        historyTrailersId.push(trailer.id);
+        localStorage.setItem('history', JSON.stringify(historyTrailersId));
       }
+    } else {
+        localStorage.setItem('history', JSON.stringify([trailer.id]));
     }
+  }
 
   function getUserTrailers() {
-      let historyTrailersId = JSON.parse(localStorage.getItem('history'));
-            if (null !== historyTrailersId) {
-        historyTrailersId = JSON.parse(historyTrailersId);
-          if (!historyTrailersId.includes(trailer.id)) {
-          historyTrailersId.push(trailer.id);
-          localStorage.setItem('history', JSON.stringify(historyTrailersId));
-          }
-      }
+    let historyTrailersId = JSON.parse(localStorage.getItem('history'));
+
+    if (null !== historyTrailersId) {
       let userTrailers = [];
       trailers.forEach(trailer => {
         if (!historyTrailersId.find(id => id === trailer.id)) {
@@ -48,43 +37,43 @@ export const App = () => {
         }
       });
 
-      // console.log(userTrailers);
-      return userTrailers
+      return userTrailers;
     }
 
-    function getRandomTrailer(trailers) {
-      if (trailers === null) return 
-     
-      const randomID = getRandomInt(trailers.length)
-      const result = trailers[randomID]
-      saveHistory(result)
-      setCurTrailer(result)
-     return result
-    }
+    return trailers;
+  }
 
-    // saveHistory({ 'id': 3 })
-    
-  // console.log())  
+  function getRandomTrailer() {
+    const trailers = getUserTrailers()
+    if (trailers === null) return
+
+    const randomID = getRandomInt(trailers.length)
+    const result = trailers[randomID]
+    saveHistory(result)
+    setCurTrailer(result)
+    return result
+  }
 
   function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
 
-    return (
-      <>
-        <Box
-          display="flex"
-          justifyContent="space-between"
-        >
-          <Btn><IoIosArrowBack size={70} /></Btn>
-          <Box width="1280" height="720px">
-            {curTrailer && <iframe width="1280" height="720" src={curTrailer.url} title="YouTube video player" frameBorder="0"   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>}
-          </Box>
-          <Btn onClick={() => {console.log(getRandomTrailer(getUserTrailers()))
-            
-          }}><IoIosArrowForward size={70}/></Btn>
+  return (
+    <>
+      <Box
+        display="flex"
+        justifyContent="space-between"
+      >
+        <Btn><IoIosArrowBack size={70}/></Btn>
+        <Box width="1280" height="720px">
+          {curTrailer &&
+          <iframe width="1280" height="720" src={curTrailer.url + '?autoplay=1&&mute=1'} title="YouTube video player" frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen playing={true}></iframe>}
         </Box>
-      </>
-    )
-  }
+        <Btn onClick={getRandomTrailer}><IoIosArrowForward size={70}/></Btn>
+      </Box>
+    </>
+  )
+}
 
